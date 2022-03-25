f87015986b601ec96fc6472107c43b24c89c0fd9
PylsaPylsa <tommybeta@gmail.com>
2022-03-25 09:23:02 +0100

    Create Policy_AllowedCountries.tf

---
+++ Terraform/Conditional Access/Policy_AllowedCountries.tf
@@ -0,0 +1,45 @@
+resource "azuread_group" "CA_LOC_AllowedCountries_Exclude" {
+  display_name     = "CA_LOC_AllowedCountries_Exclude"
+  description      = "Excludes users from the CA policy \"LOC | Allowed Countries\""
+  owners           = [data.azuread_client_config.current.object_id]
+  security_enabled = true
+}
+
+resource "time_sleep" "wait_CA_MFA_AllUsers_Exclude" {
+  depends_on = [azuread_group.CA_MFA_AllUsers_Exclude]
+  create_duration = "15s"
+}
+
+resource "azuread_conditional_access_policy" "MFA_AllUsers" {
+  display_name = "LOC | Allowed Countries"
+  state        = "enabledForReportingButNotEnforced"
+
+  conditions {
+    client_app_types = ["all"]
+
+    applications {
+      included_applications = ["All"]
+    }
+
+    locations {
+      included_locations = ["All"]
+      excluded_locations = [azuread_named_location.safecountries.id]
+    }
+
+    platforms {
+      included_platforms = ["all"]
+    }
+
+    users {
+      included_users  = ["All"]
+      excluded_groups = [azuread_group.CA_LOC_AllowedCountries_Exclude.id]
+    }
+  }
+
+  grant_controls {
+    operator          = "OR"
+    built_in_controls = ["block"]
+  }
+
+  depends_on = [time_sleep.wait_CA_LOC_AllowedCountries_Exclude]
+}
