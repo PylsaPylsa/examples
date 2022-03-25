d06fa2726ea0cca083a924f569ccb25ee6b725a8
PylsaPylsa <tommybeta@gmail.com>
2022-03-25 14:58:46 +0100

    Update Policy_BlockLegacyAuth.tf

--- Terraform/Conditional Access/Policy_BlockLegacyAuth.tf
+++ Terraform/Conditional Access/Policy_BlockLegacyAuth.tf
@@ -1,16 +1,16 @@
-resource "azuread_group" "CA_LOC_BlockLegacyAuth_Exclude" {
-  display_name     = "CA_LOC_BlockLegacyAuth_Exclude"
+resource "azuread_group" "CA_AUTH_BlockLegacyAuth_Exclude" {
+  display_name     = "CA_AUTH_BlockLegacyAuth_Exclude"
   description      = "Excludes users from the CA policy \" AUTH | Block Legacy Authentication\""
   owners           = [data.azuread_client_config.current.object_id]
   security_enabled = true
 }
 
-resource "time_sleep" "wait_CA_LOC_BlockLegacyAuth_Exclude" {
-  depends_on      = [azuread_group.CA_LOC_BlockLegacyAuth_Exclude]
+resource "time_sleep" "wait_CA_AUTH_BlockLegacyAuth_Exclude" {
+  depends_on      = [azuread_group.CA_AUTH_BlockLegacyAuth_Exclude]
   create_duration = "15s"
 }
 
-resource "azuread_conditional_access_policy" "LOC_BlockLegacyAuth" {
+resource "azuread_conditional_access_policy" "AUTH_BlockLegacyAuth" {
   display_name = "AUTH | Block Legacy Authentication"
   state        = var.CAPolicyState
 
@@ -31,7 +31,7 @@ resource "azuread_conditional_access_policy" "LOC_BlockLegacyAuth" {
 
     users {
       included_users  = [var.includeCAGroup]
-      excluded_groups = [azuread_group.CA_LOC_BlockLegacyAuth_Exclude.id]
+      excluded_groups = [azuread_group.CA_AUTH_BlockLegacyAuth_Exclude.id]
     }
   }
 
@@ -40,5 +40,5 @@ resource "azuread_conditional_access_policy" "LOC_BlockLegacyAuth" {
     built_in_controls = ["block"]
   }
 
-  depends_on = [time_sleep.wait_CA_LOC_BlockLegacyAuth_Exclude]
+  depends_on = [time_sleep.wait_CA_AUTH_BlockLegacyAuth_Exclude]
 }
